3dc97fe25533e84c072e6f45546190f619b895ec
MeowGMS <35658722+MeowGMS@users.noreply.github.com>
2018-08-14 19:36:59 +0300

    Update votemute.js

--- commands/votemute.js
+++ commands/votemute.js
@@ -106,7 +106,10 @@ module.exports.run = async (client, message, args, dbMessage) => {
                             let punishTime = parseInt(messageArray[2], 10);
 
                             let embed = new Discord.RichEmbed()
-                                .addField(`Информация`, `**${userForPunish} был замучен на \`${punishTime}\` ${declOfNum(punishTime, titlesArray2)}**\n\n**Соотношение за/против: ${msgs.in_favor} \\✅/ ${msgs.against} \\❌**\n\n**Начал голосование:** ${message.author}`)
+                                .addField(`Информация`, `**${userForPunish}** лишился прав общаться в голосовых и текстовых каналах\n\n**Начал голосование:** ${message.author}`)
+                                .addField(`За`, `${msgs.in_favor}`, true)
+                                .addField(`Против`, `${msgs.against}`, true)
+                                .addField(`Время`, `${punishTime}`)
                                 .addField(`Причина`, `\`\`\`fix\n${punishReason}\`\`\``)
                                 .setColor(`#00D11A`)
                                 .setThumbnail(`${userForPunish.avatarURL}`)
@@ -144,7 +147,9 @@ module.exports.run = async (client, message, args, dbMessage) => {
 
                         if (msgs.in_favor <= msgs.against) {
                             let embed = new Discord.RichEmbed()
-                                .addField(`Информация`, `**${userForPunish} не был замучен\n\nСоотношение за/против: ${msgs.in_favor} \\✅/ ${msgs.against} \\❌\n\nНачал голосование: ${message.author}**`)
+                                .addField(`Информация`, `**Для мута ${userForPunish} недостаточно голосов\n\nНачал голосование: ${message.author}**`)
+                                .addField(`За`, `${msgs.in_favor}`, true)
+                                .addField(`Против`, `${msgs.against}`, true)
                                 .setColor(`#F01717`)
                                 .setFooter(`${m.guild.name}`)
                                 .setTimestamp()
