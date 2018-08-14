e92163af4c69e45c6b5bce5d4c84daa902b426e1
MeowGMS <35658722+MeowGMS@users.noreply.github.com>
2018-08-14 14:37:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const prefix = 'v.'
 
 const messageSchema = new Schema({
     id: String,
+    msgChannelID: String,
     in_favor: Number,
     against: Number,
     punishTime: Number,
@@ -17,7 +18,10 @@ const messageSchema = new Schema({
     authorID: String,
     punishableID: String,
     punishReason: String,
-    ended: Boolean
+    ended: Boolean,
+    endedVoting: Boolean,
+    in_favorIDs: [String],
+    againstIDs: [String]
 });
 const dbMessage = mongoose.model('message', messageSchema);
 
@@ -81,11 +85,220 @@ client.on("ready", async () => {
                             msgs.save()
                         }, msgs.unmuteTime - nowTimeStamp);
                     }
-                    
+
                 });
             }
         });
+
     });
+
+    client.guilds.get('468327359687426049').members.forEach((member) => {
+        dbMessage.findOne({
+            punishableID: member.user.id,
+            endedVoting: false
+        }).then((voting) => {
+            if (voting) {
+                dbMessage.findOne({
+                    punishableID: member.user.id,
+                    endedVoting: false
+                }, function(err, msgs) {
+
+                    if (nowTimeStamp >= msgs.resultsTime) {
+                        console.log(`Голосование закончено`);
+                        let msg = client.channels.get(msgs.msgChannelID).fetchMessage(msgs.id);
+                        let in_favorCount msg.reactions.get('✅').count;
+                        let againstCount msg.reactions.get('❌').count;
+
+
+
+                        setTimeout(() => {
+
+                            dbMessage.findOne({
+                                punishableID: member.user.id,
+                                endedVoting: false
+                            }, function(err, msgs) {
+                                msgs.endedVoting = true;
+                                msgs.save();
+                            });
+
+                            dbMessage.findOne({
+                                punishableID: member.user.id,
+                                ended: false
+                            }, function(err, msgs) {
+                                if (in_favorCount > againstCount) {
+
+                                    let titlesArray2 = [''];
+
+                                    if (lastSymbol == 'm' || lastSymbol == 'м') {
+                                        titlesArray2 = ['минуту', 'минуты', 'минут'];
+                                        let multNum = 60000;
+                                        let punishTime = parseInt(messageArray[2].slice(0, -1), 10);
+                                    } else if (lastSymbol == 'h' || lastSymbol == 'ч') {
+                                        titlesArray2 = ['час', 'часа', 'часов'];
+                                        let multNum = 3600000;
+                                        let punishTime = parseInt(messageArray[2].slice(0, -1), 10);
+                                    } else {
+                                        titlesArray2 = ['минуту', 'минуты', 'минут'];
+                                        let multNum = 60000;
+                                        let punishTime = parseInt(messageArray[2], 10);
+                                    }
+
+                                    let embed = new Discord.RichEmbed()
+                                        .addField(`Информация`, `**${userForPunish} был замучен на \`${punishTime}\` ${declOfNum(msgs.punishTime, titlesArray2)}**\n\n**Соотношение за/против: ${in_favorCount} \\✅/ ${against} \\❌**\n\n**Начал голосование:** ${msg.author}`)
+                                        .addField(`Причина`, `\`\`\`fix\n${punishReason}\`\`\``)
+                                        .setColor(`#00D11A`)
+                                        .setFooter(`${msg.guild.name}`)
+                                        .setTimestamp()
+
+                                    m.edit(`\`\`\` \`\`\``, {
+                                        embed
+                                    });
+
+                                    m.guild.members.get(userForPunish.id).addRole(config.muteRoleID);
+
+                                    let notTimestamp = Date.now();
+
+                                    msgs.unmuteTime = notTimestamp + msgs.punishTime;
+
+                                    msgs.save();
+                                    setTimeout(() => {
+                                        m.guild.members.get(msgs.punishableID).removeRole(config.muteRoleID);
+
+                                        console.log(`${client.fetchUser(msgs.punishableID).tag} был размучен`);
+                                    }, punishTime * 5000);
+                                    //}, punishTime * multNum);
+
+                                    msg.clearReactions();
+
+                                }
+
+                                if (in_favorCount <= againstCount) {
+                                    let embed = new Discord.RichEmbed()
+                                        .addField(`Информация`, `**${member.user} не был замучен\n\nСоотношение за/против: ${in_favorCount} \\✅/ ${againstCount} \\❌\n\nНачал голосование: ${msg.author}**`)
+                                        .setColor(`#F01717`)
+                                        .setFooter(`${msg.guild.name}`)
+                                        .setTimestamp()
+
+                                    msg.edit(`\`\`\` \`\`\``, {
+                                        embed
+                                    });
+
+                                    msg.clearReactions();
+                                }
+
+
+                            });
+
+                            dbMessage.findOne({
+                                punishableID: member.user.id,
+                                ended: false
+                            }, function(err, msgs) {
+                                msgs.against = againstCount;
+                                msgs.in_favor = in_favorCount;
+                                msgs.ended = true;
+                                msgs.save();
+                            });
+                        }, 2000);
+
+                    } else {
+
+                        console.log(`Голосование запущено заного и будет закончено через ${Math.floor((msgs.resultsTime - nowTimeStamp) / 1000)}`);
+                        setTimeout(() => {
+
+                            dbMessage.findOne({
+                                punishableID: member.user.id,
+                                endedVoting: false
+                            }, function(err, msgs) {
+                                msgs.endedVoting = true;
+                                msgs.save();
+                            });
+
+                            dbMessage.findOne({
+                                punishableID: member.user.id,
+                                ended: false
+                            }, function(err, msgs) {
+                                if (in_favorCount > againstCount) {
+
+                                    let titlesArray2 = [''];
+
+                                    if (lastSymbol == 'm' || lastSymbol == 'м') {
+                                        titlesArray2 = ['минуту', 'минуты', 'минут'];
+                                        let multNum = 60000;
+                                        let punishTime = parseInt(messageArray[2].slice(0, -1), 10);
+                                    } else if (lastSymbol == 'h' || lastSymbol == 'ч') {
+                                        titlesArray2 = ['час', 'часа', 'часов'];
+                                        let multNum = 3600000;
+                                        let punishTime = parseInt(messageArray[2].slice(0, -1), 10);
+                                    } else {
+                                        titlesArray2 = ['минуту', 'минуты', 'минут'];
+                                        let multNum = 60000;
+                                        let punishTime = parseInt(messageArray[2], 10);
+                                    }
+
+                                    let embed = new Discord.RichEmbed()
+                                        .addField(`Информация`, `**${userForPunish} был замучен на \`${punishTime}\` ${declOfNum(msgs.punishTime, titlesArray2)}**\n\n**Соотношение за/против: ${in_favorCount} \\✅/ ${against} \\❌**\n\n**Начал голосование:** ${msg.author}`)
+                                        .addField(`Причина`, `\`\`\`fix\n${punishReason}\`\`\``)
+                                        .setColor(`#00D11A`)
+                                        .setFooter(`${msg.guild.name}`)
+                                        .setTimestamp()
+
+                                    m.edit(`\`\`\` \`\`\``, {
+                                        embed
+                                    });
+
+                                    m.guild.members.get(userForPunish.id).addRole(config.muteRoleID);
+
+                                    let notTimestamp = Date.now();
+
+                                    msgs.unmuteTime = notTimestamp + msgs.punishTime;
+
+                                    msgs.save();
+                                    setTimeout(() => {
+                                        m.guild.members.get(msgs.punishableID).removeRole(config.muteRoleID);
+
+                                        console.log(`${client.fetchUser(msgs.punishableID).tag} был размучен`);
+                                    }, punishTime * 5000);
+                                    //}, punishTime * multNum);
+
+                                    msg.clearReactions();
+
+                                }
+
+                                if (in_favorCount <= againstCount) {
+                                    let embed = new Discord.RichEmbed()
+                                        .addField(`Информация`, `**${member.user} не был замучен\n\nСоотношение за/против: ${in_favorCount} \\✅/ ${againstCount} \\❌\n\nНачал голосование: ${msg.author}**`)
+                                        .setColor(`#F01717`)
+                                        .setFooter(`${msg.guild.name}`)
+                                        .setTimestamp()
+
+                                    msg.edit(`\`\`\` \`\`\``, {
+                                        embed
+                                    });
+
+                                    msg.clearReactions();
+                                }
+
+
+                            });
+
+                            dbMessage.findOne({
+                                punishableID: member.user.id,
+                                ended: false
+                            }, function(err, msgs) {
+                                msgs.against = againstCount;
+                                msgs.in_favor = in_favorCount;
+                                msgs.ended = true;
+                                msgs.save();
+                            });
+                        }, msgs.resultsTime - nowTimeStamp);
+                    }
+
+                });
+            }
+        });
+
+    });
+
 });
 
 client.on('messageReactionAdd', (reaction, user) => {
