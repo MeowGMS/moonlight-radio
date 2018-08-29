b819cfcabe90e5dba58363059b43a7967b70be1d
MeowGMS <35658722+MeowGMS@users.noreply.github.com>
2018-08-29 17:20:39 +0300

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -8,15 +8,21 @@ module.exports.run = async (client, message, args) => {
 
     let connection = client.voiceConnections.get(message.guild.id);
 
-    if (!connection) return errors.botNotInChannel(message.channel, message);
+    if (!connection && !message.voiceChannel) return errors.botNotInChannel(message.channel, message);
 
     connection.disconnect()
 
+    if (message.guild.iconURL == null) {
+        guildIcon = 'https://cdn.discordapp.com/attachments/484360305837735949/484360414277402624/freeios7.com_apple_wallpaper_rainbow-blurs_iphone5.jpg'
+    } else {
+        guildIcon = message.guild.iconURL;
+    }
+
     let embed = new Discord.RichEmbed()
-        .setAuthor(`Бот отключён`, `${message.guild.iconURL}`)
+        .setAuthor(`Бот отключён`, `${guildIcon}`)
         .addField(`Использовал(а) команду`, `${message.author}`, true)
         .setColor(config.moonlightColor)
-        .setFooter(`Отключить бота - ${prefix}stop`)
+        .setFooter(`${prefix}stop - Отключение бота`)
         .setTimestamp()
 
     message.channel.send({
